2ef00ec6b96fe60146bb9979a58b22b55951268c
chituenguyen <tuenguyen553@gmail.com>
2023-08-04 11:27:08 +0700

    fix build

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import { ReactQueryProvider } from "./ReactQueryProvideer";
+import { ReactQueryProvider } from "../const/ReactQueryProvideer";
 
 function MyApp({ Component, pageProps }) {
   return (
